5b3ac2676ebfa59f5c34b0fbaa5bd502b9788c25
Ceren Çiçek <53222595+crncck@users.noreply.github.com>
2020-05-26 16:35:17 +0300

    Update ceng_2034_final_answer.py

--- ceng_2034_final_answer.py
+++ ceng_2034_final_answer.py
@@ -5,19 +5,21 @@ import os
 import requests
 import re
 import uuid
+import os.path
+from os import path
+import urllib.parse
 from urllib.parse import urlparse
 
 #create a child process and print its pid
-def childParent():
-	n = os.fork()
-	if(n > 0):
-		print("PID of the parent process: {}" .format(os.getpid()))
-	else:
-		print("PID of the child process: {}".format(os.getpid()))
+pid = os.fork()
+if(pid > 0):
+	print("PID of the parent process: {}" .format(os.getpid()))
+else:
+	print("PID of the child process: {}".format(os.getpid()))
 
-childParent()
 
-#download the files via given URL list
+
+#download the files via given URL list with child process
 urls = ['http://wiki.netseclab.mu.edu.tr/images/thumb/f/f7/MSKU-BlockchainResearchGroup.jpeg/300px-MSKU-BlockchainResearchGroup.jpeg',
 'https://upload.wikimedia.org/wikipedia/tr/9/98/Mu%C4%9Fla_S%C4%B1tk%C4%B1_Ko%C3%A7man_%C3%9Cniversitesi_logo.png',
 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Hawai%27i.jpg/1024px-Hawai%27i.jpg',
@@ -26,17 +28,35 @@ urls = ['http://wiki.netseclab.mu.edu.tr/images/thumb/f/f7/MSKU-BlockchainResear
 
 def download_file(url, file_name):
 	r = requests.get(url, allow_redirects=True)
-	file = file_name if file_name else str(uuid.uuid4())
+	a = path.exists(file_name)
+	if a == 0:
+		file = file_name if file_name else str(uuid.uuid4())
+		
+	else:
+		file_name = file_name.split('.')[0]+'(2).'+file_name.split('.')[1]
+		file = file_name
+		
 	open(file, "wb").write(r.content)
 
-for u in urls:
-	filename = urlparse(u)
-	download_file(u, os.path.basename(filename.path))
+def childProcess(pid):
 
+	children = []
 
+	if pid > 0:
+		children.append(pid)
+	else:
+		#the child process will download the files
+		for u in urls:
+			filename = urlparse(u)
+			download_file(u, os.path.basename(filename.path))
+		os._exit(0)
 
+	#to avoid orphan processes 
+	for i, proc in enumerate(children):
+		os.waitpid(proc, 0)
+	print("Parent process is closing")
 
-
+childProcess(pid)
 
 
 
